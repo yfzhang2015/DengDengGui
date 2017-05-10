dc4083d573234f43bed8ed141dc292e00b3c13bb
桂素伟 <axzxs2001@163.com>
2017-05-10 14:06:14 +0800

    仓储模式中添加菜单查询，登录，修改密码。

--- DengDengGui/GeneralBusinessRepository/BusinessForSqlServerRepository.cs
+++ DengDengGui/GeneralBusinessRepository/BusinessForSqlServerRepository.cs
@@ -74,6 +74,51 @@ namespace GeneralBusinessRepository
             var idParameter = new SqlParameter() { Value = id, ParameterName = "@id" };
             return _sqlHelper.ChangeData(sql, idParameter);
         }
+        /// <summary>
+        /// 登录，并返回该用户合部信息
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <returns></returns>
+        public Dictionary<string, dynamic> Login(string userName, string password)
+        {
+            var sql = "select * from users where username=@username and password=@password";
+            var userNameParameter = new SqlParameter() { Value = userName, ParameterName = "@username" };
+            var passwordParameter = new SqlParameter() { Value = password, ParameterName = "@password" };
+            var result = _sqlHelper.QueryList(sql, userNameParameter, passwordParameter);
+            if (result != null && result.Count > 0)
+            {
+                return result[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="newPassword">新密码</param>
+        /// <returns></returns>
+        public bool ModifyPassword(string userName,string newPassword)
+        {
+            var sql = "update  users set password=@newpassword where username=@username ";
+            var userNameParameter = new SqlParameter() { Value = userName, ParameterName = "@username" };         
+            var newPasswordParameter = new SqlParameter() { Value = newPassword, ParameterName = "@newpassword" };
+            var result = _sqlHelper.ChangeData(sql, userNameParameter, newPasswordParameter);
+            if (result > 0)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+            else
+            {
+                return null;
+            }
+        }
         #endregion
 
         #region 角色管理
@@ -125,7 +170,45 @@ namespace GeneralBusinessRepository
         }
         #endregion
 
-       
+        #region 菜单管理
+        /// <summary>
+        /// 按用户名查询菜单
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <returns></returns>
+        public List<Dictionary<string, dynamic>> GetMenusByUserName(string userName)
+        {
+            var sql = $@"SELECT rolemodules.menuid ,menus.name,rolemodules.moduleid,modules.name
+FROM    users
+        JOIN userroles ON users.id = userroles.userid
+        JOIN roles ON userroles.roleid = roles.id
+        JOIN rolemodules ON roles.id = rolemodules.roleid
+		JOIN dbo.Menus ON Menus.id=rolemodules.menuid
+        JOIN ( SELECT   id ,
+                        name ,
+                        menuid
+               FROM     billmodules
+               UNION ALL
+               SELECT   id ,
+                        name ,
+                        menuid
+               FROM     QueryModules
+               UNION ALL
+               SELECT   id ,
+                        name ,
+                        menuid
+               FROM     ChartModules
+             ) AS modules ON modules.menuid = rolemodules.menuid
+                             AND modules.id = rolemodules.moduleid
+							 WHERE users.username=@username";
+            var userNameParameter = new SqlParameter() { Value = userName, ParameterName = "@username" };
+            return _sqlHelper.QueryList(sql, userNameParameter);
+
+        }
+
+
+
+        #endregion
 
 
     }
