41cfa38b15d0e24c4323f6470e52edcccf830d7e
桂素伟 <axzxs2001@163.com>
2017-05-23 20:34:12 +0800

    重构用户和角色代码。 添加菜单模块页面。

--- DengDengGui/GeneralBusinessSystem/Controllers/ManagementController.cs
+++ DengDengGui/GeneralBusinessSystem/Controllers/ManagementController.cs
@@ -23,19 +23,33 @@ namespace GeneralBusinessSystem.Controllers
         }
 
         #region User操作
+        /// <summary>
+        /// 用户页面
+        /// </summary>
+        /// <returns></returns>
         [Route("users")]
-        public ActionResult UserIndex()
+        public ActionResult User()
         {
             return View(_businessRepository.GetUsers());
         }
+        /// <summary>
+        /// 查询用户
+        /// </summary>
+        /// <param name="queryName"></param>
+        /// <returns></returns>
         [Route("queryusers")]
         public ActionResult QueryUser(string queryName)
         {
             return new JsonResult(_businessRepository.GetUsers(queryName));
         }
-
-        [HttpPost]
-        [Route("adduser")]
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="name">名称</param>
+        /// <returns></returns>
+        [HttpPost("adduser")]
         public bool UserAdd(string userName, string password, string name)
         {
             try
@@ -50,9 +64,15 @@ namespace GeneralBusinessSystem.Controllers
             }
         }
 
-
-        [HttpPost]
-        [Route("modifyuser")]
+        /// <summary>
+        /// 修改用户
+        /// </summary>
+        /// <param name="ID">ID</param>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="name">名称</param>
+        /// <returns></returns>
+        [HttpPost("modifyuser")]
         public bool UserModify(int ID, string userName, string password, string name)
         {
             try
@@ -66,9 +86,12 @@ namespace GeneralBusinessSystem.Controllers
                 return false;
             }
         }
-
-        [HttpPost]
-        [Route("deleteuser")]
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        [HttpPost("deleteuser")]       
         public bool UserDelete(int ID)
         {
 
@@ -84,7 +107,6 @@ namespace GeneralBusinessSystem.Controllers
         }
         #endregion
 
-
         #region Role管理
         /// <summary>
         /// 添加角色 
@@ -95,7 +117,6 @@ namespace GeneralBusinessSystem.Controllers
             return View();
         }
 
-        #region 角色
         [HttpGet("roles")]
         public IActionResult Role()
         {
@@ -108,23 +129,40 @@ namespace GeneralBusinessSystem.Controllers
             var list = _businessRepository.GetRoles();
             return new JsonResult(list, new Newtonsoft.Json.JsonSerializerSettings());
         }
-
+        /// <summary>
+        /// 添加角色
+        /// </summary>
+        /// <param name="rolename">角色名称</param>
+        /// <returns></returns>
         [HttpPost("addrole")]
         public bool AddRole(string rolename)
         {
             return _businessRepository.AddRole(rolename) > 0 ? true : false;
         }
+        /// <summary>
+        /// 修改角色 
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="rolename">角色名称</param>
+        /// <returns></returns>
         [HttpPost("modifyrole")]
         public bool ModifyRole(int id, string rolename)
         {
             return _businessRepository.ModifyRole(id, rolename) > 0 ? true : false;
         }
+        /// <summary>
+        /// 删除角色
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns></returns>
         [HttpPost("deleterole")]
         public bool DeleteRole(int id)
         {
             return _businessRepository.RemoveRole(id) > 0 ? true : false;
         }
         #endregion
-        #endregion
+
+
+   
     }
 }
