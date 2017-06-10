f5af845f81bca809b9b14c16ac44c97c6e3950d6
桂素伟 <axzxs2001@163.com>
2017-06-10 08:39:05 +0800

    添加js中，string,array,date扩展函数扩展属性。

---
+++ DengDengGui/GeneralBusinessSystem/wwwroot/js/PrototypeExtensions.js
@@ -0,0 +1,53 @@
+﻿/*************************************
+字符串扩展属性
+***************************************/
+//字符串除空格
+String.prototype.trim = function () {
+    return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+}
+//字符是否包含子字符串
+String.prototype.contains = function (subString) {
+    return this.indexOf(subString)>-1?true:false;
+}
+/**************************************/
+
+
+
+/*************************************
+日期扩展属性
+***************************************/
+
+//日期格式化函数
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1,                 //月份   
+        "d+": this.getDate(),                    //日   
+        "h+": this.getHours(),                   //小时   
+        "m+": this.getMinutes(),                 //分   
+        "s+": this.getSeconds(),                 //秒   
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度   
+        "S": this.getMilliseconds()             //毫秒   
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+/**************************************/
+
+
+
+/*************************************
+数组扩展属性
+***************************************/
+
+//数组包含
+Array.prototype.contains = function (needle) {
+    for (i in this) {
+        if (this[i] == needle) return true;
+    }
+    return false;
+}
+/**************************************/
