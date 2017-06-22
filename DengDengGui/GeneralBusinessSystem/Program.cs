7543c4d5c307f136892461c1e7b02f036489c302
桂素伟 <axzxs2001@163.com>
2017-06-22 16:57:12 +0800

    添加启动IP:Port配置文件hosting.json

--- DengDengGui/GeneralBusinessSystem/Program.cs
+++ DengDengGui/GeneralBusinessSystem/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace GeneralBusinessSystem
 {
@@ -12,11 +13,16 @@ namespace GeneralBusinessSystem
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("hosting.json", optional: true)
+          .Build();
             //支持中文
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
