f0cc23be6f817a23b807475154e96c19691b8335
桂素伟 <axzxs2001@163.com>
2017-08-30 08:09:59 +0800

    迁移项目到2.0

--- DengDengGui/GeneralBusinessSystem/Program.cs
+++ DengDengGui/GeneralBusinessSystem/Program.cs
@@ -6,30 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using System.Text;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore;
 
 namespace GeneralBusinessSystem
 {
     public class Program
     {
+   
+
         public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
-          .SetBasePath(Directory.GetCurrentDirectory())
-          .AddJsonFile("hosting.json", optional: true)
-          .Build();
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("hosting.json", optional: true)
+        .Build();
             //支持中文
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseConfiguration(config)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args, config).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args, IConfigurationRoot config) =>
+            WebHost.CreateDefaultBuilder(args)
+               .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
--- DengDengGui/GeneralBusinessSystem/Startup.cs
+++ DengDengGui/GeneralBusinessSystem/Startup.cs
@@ -20,17 +20,12 @@ namespace GeneralBusinessSystem
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         /// <summary>
         /// 
