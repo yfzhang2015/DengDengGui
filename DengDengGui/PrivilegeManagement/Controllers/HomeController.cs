0d18a9146ceb86662355120d6b54b0e44e199a2f
桂素伟 <axzxs2001@163.com>
2017-08-30 16:25:03 +0800

    asp.net 2.0 权限验证新框架。

---
+++ DengDengGui/PrivilegeManagement/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PrivilegeManagement.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+namespace PrivilegeManagement.Controllers
+{
+ 
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            TempData["returnUrl"] = returnUrl;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUser loginUser, string returnUrl = null)
+        {
+            if (loginUser.UserName == "aaa" && loginUser.Password == "bbb")
+            {
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.Name, loginUser.UserName));
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                if (returnUrl == null)
+                {
+                    returnUrl = TempData["returnUrl"]?.ToString();
+                }
+                if (returnUrl != null)
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+            else
+            {
+                const string badUserNameOrPasswordMessage = "用户名或密码错误！";
+                return BadRequest(badUserNameOrPasswordMessage);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+    public class LoginUser
+    {
+        public string UserName
+        { get; set; }
+        public string Password
+        { get; set; }
+    }
+}
---
+++ DengDengGui/PrivilegeManagement/PermissionMiddleware/PermissionMiddleware.cs
@@ -0,0 +1,74 @@
+﻿
+
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PrivilegeManagement.Middleware
+{
+    /// <summary>
+    /// 权限中间件
+    /// </summary>
+    public class PermissionMiddleware
+    {
+        /// <summary>
+        /// 管道代理对象
+        /// </summary>
+        private readonly RequestDelegate _next;
+        /// <summary>
+        /// 权限中间件的配置选项
+        /// </summary>
+        private readonly PermissionMiddlewareOption _option;
+
+        /// <summary>
+        /// 用户权限集合
+        /// </summary>
+        internal static List<dynamic> _userPermissions;
+
+        /// <summary>
+        /// 权限中间件构造
+        /// </summary>
+        /// <param name="next">管道代理对象</param>
+        /// <param name="permissionResitory">权限仓储对象</param>
+        /// <param name="option">权限中间件配置选项</param>
+        public PermissionMiddleware(RequestDelegate next, PermissionMiddlewareOption option)
+        {
+            _option = option;
+            _next = next;
+            LoadUserPermissions();
+        }
+        /// <summary>
+        /// 获取用户权限
+        /// </summary>
+        void LoadUserPermissions()
+        {
+            if (_userPermissions == null)
+            {
+                _userPermissions = new List<dynamic>();
+
+            }
+        }
+        /// <summary>
+        /// 调用管道
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext context)
+        {
+            var questUrl = context.Request.Path.Value;
+            var userName = context.User.Identity.Name;
+            var isAuthenticated = context.User.Identity.IsAuthenticated;
+
+            ////无权限跳转到拒绝页面
+            //context.Response.Redirect(_option.NoPermissionAction);
+
+            return this._next(context);
+        }
+
+    }
+}
